dfc67b220d18f61083746c28fe7b755d0bd41427
Yan Lyra <yan.lyra@growth.sale>
2023-05-09 17:48:29 -0300

    wip

--- src/pages/live-stream/criar/index.tsx
+++ src/pages/live-stream/criar/index.tsx
@@ -1,11 +1,21 @@
 import { NextPageWithLayout } from "@/@types/next";
+import BreadCrumb from "@/components/Common/BreadCrumb";
 import Layout from "@/containers/Layout";
 import { withSSRAuth } from "@/utils/withSSRAuth";
+import Head from "next/head";
 
 interface Props {}
 
 const Page: NextPageWithLayout<Props> = () => {
-  return <h1>hello world</h1>;
+  return (
+    <div className="page-content">
+      <Head>
+        <title>Gerenciamento da Live - Dashboard</title>
+      </Head>
+
+      <BreadCrumb title="Nova Live" pageTitle="Ecommerce" />
+    </div>
+  );
 };
 
 export const getServerSideProps = withSSRAuth<Props>(async (ctx) => {
